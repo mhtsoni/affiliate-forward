104cd963a09cea3a3ae9b33152e0b728b7fd853c
Mohit Soni <mohit19699soni@gmail.com>
2021-01-28 17:22:46 +0530

    Testing Insta Post Feature

--- instaBot.js
+++ instaBot.js
@@ -1,18 +1,21 @@
 
 const Instagram = require('instagram-web-api')
+const nodeHtmlToImage = require('node-html-to-image') 
 module.exports.IntaPost=async (dealText,caption) => {
     const client = new Instagram({ username:"deals_point_official", password:"Ranasoni1@" });
-    const nodeHtmlToImage = require('node-html-to-image') 
     nodeHtmlToImage({
-    output: './images/deal.jpg',
-    html: `<div style="font-size:4vw; background-color:black;min-height:100vh;color:white; display:flex; flex-direction:row; align-items:center;justify-content:center;">
-            <p>${dealText}</p>
-        </div>`
-    })
-    .then(async () =>{
-    const photo ='./images/deal.jpg';
-    await client.login()
-    const { media } = await client.uploadPhoto({ photo: photo, caption: 'Best Online Shopping Deals Daily, Check Bio to Join The WhatsApp Group #shoppingdeals #online #shopping #moneysavings', post: 'feed' })
-    console.log(`https://www.instagram.com/p/${media.code}/`)
-  })
+        output: './images/deal.jpg',
+        html: `<div style="font-size:4vw; background-color:black;min-height:100vh;color:white; display:flex; flex-direction:row; align-items:center;justify-content:center;">
+                <p>${dealText}</p>
+            </div>`
+      }).then(async ()=>{
+        await client.login()
+        client.uploadPhoto({ photo: './images/deal.jpg', caption: 'Best Online Shopping Deals Daily, Check Bio to Join The WhatsApp Group #shoppingdeals #online #shopping #moneysavings', post: 'feed' })
+        .then((res)=>{
+            console.log(`https://www.instagram.com/p/`+res.media.code);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+      })
 }
