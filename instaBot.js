65be84323bc0679efb9c7bba3a7991e87f28727a
Mohit Soni <mohit19699soni@gmail.com>
2021-01-28 17:45:47 +0530

    Testing Insta Post Feature

--- instaBot.js
+++ instaBot.js
@@ -2,7 +2,8 @@
 const Instagram = require('instagram-web-api')
 const nodeHtmlToImage = require('node-html-to-image') 
 module.exports.IntaPost=async (dealText,caption) => {
-    const client = new Instagram({ username:"deals_point_official", password:"Ranasoni1@" });
+    const client = new Instagram({ username:"deals_point_official", password:"Ranasoni1@",
+	browserArgs: ['--no-sandbox','disable-setuid-sandbox'] });
     nodeHtmlToImage({
         output: './images/deal.jpg',
         html: `<div style="font-size:4vw; background-color:black;min-height:100vh;color:white; display:flex; flex-direction:row; align-items:center;justify-content:center;">
